17a5246342aa566fc80847d42ed4a5734564074e
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 09:39:35 -0300

    feat: add messages erros and action on create task form

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,21 +1,27 @@
 @extends('layout')
-@section('content')
-<form>
+@section('create-task-form')
+
+<form action="{{ route('task.store')}}" method="POST">
+    @csrf
     <div class="container">
-        <h3 class="text-4xl font-bold mb-4">Create a task</h3>
-        <div class="flex flex-col border rounded p-4 max-w-[80%] bg-slate-800 border-slate-900 gap-2">
+        <h3 class="mb-4 text-4xl font-bold">Create a task</h3>
+        <div class="flex flex-col  rounded p-4 max-w-[80%] bg-slate-800 gap-2">
 
             <label for="title" class="text-base font-semibold">Title <span class="text-red-600">*</span></label>
             <input type="text" id="title" name="title" placeholder="Task title..." class="p-2 rounded bg-slate-600">
+            @error('title')
+            <div class="text-sm text-red-600">
+                {{ $message }}
+            </div>
+            @enderror
 
             <label for="description" class="text-base font-semibold">Description</label>
             <textarea id="description" name="description" placeholder="A short description..."
-                class="rounded p-2 bg-slate-600 ">
-            </textarea>
+                class="p-2 rounded bg-slate-600 "></textarea>
 
 
             <button type="submit"
-                class=" bg-emerald-700 rounded py-1 px-2 max-w-32 hover:bg-emerald-600 transition mt-2">Create</button>
+                class="px-2 py-1 mt-2 transition rounded bg-emerald-700 max-w-32 hover:bg-emerald-600">Create</button>
         </div>
 </form>
 @endsection
