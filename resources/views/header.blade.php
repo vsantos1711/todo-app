b8ab55e4dd7ebbbd1a3545742b71a34359878565
vsantos1711 <vsantos067100@gmail.com>
2024-02-13 23:39:22 -0300

    feat: create header component

---
+++ resources/views/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="container flex justify-between items-center py-2 mb-5">
+    <a href="#" class="text-3xl font-bold ">Todo app</a>
+    <ul class="flex gap-4 text-lg">
+        <li class="hover:text-red-500 transition"><a href="#">Login</a></li>
+        <li class="hover:text-red-500 transition"><a href="#">Create</a></li>
+    </ul>
+</nav>
