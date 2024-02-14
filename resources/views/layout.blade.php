a94b7f122eed0a6fd5b832efc541d0fea2c17f19
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 09:39:17 -0300

    style: change title and add favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,19 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel CRUD App</title>
+    <link rel="shortcut icon" href="{{asset('favicon.svg')}}" type="image/x-icon">
+    <title>T.O.D.O</title>
 
     <!-- Styles -->
     @vite('resources/css/app.css')
 </head>
 
-<body class="text-slate-200 bg-gray-900 dark:bg-dots-lighter">
-    <header class="bg-slate-800 bg-opacity-90">
-        @include('header')
-    </header>
+<body class="bg-gray-900 text-slate-200 dark:bg-dots-lighter">
+    @include('header')
     <main class="min-h-screen">
-        @yield('content')
+        @yield('create-task-form')
     </main>
 </body>
 
 </html>
+a
