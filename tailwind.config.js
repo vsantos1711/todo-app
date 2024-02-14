ce8f48decfadb9eecc7ca1b651aad54ee8220742
vsantos1711 <vsantos067100@gmail.com>
2024-02-13 21:56:23 -0300

    style: add standard container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "2rem",
+                sm: "3rem",
+                lg: "9rem",
+                xl: "11rem",
+                "2xl": "13rem",
+            },
+            screens: {
+                "2xl": "1400px",
+            },
+        },
     },
     plugins: [],
 };
