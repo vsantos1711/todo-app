cc21bd8d1a79804683070bf045d2881076edd170
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 09:37:22 -0300

    style: move all header HTML to component

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,9 @@
-<nav class="container flex justify-between items-center py-2 mb-5">
-    <a href="#" class="text-3xl font-bold ">Todo app</a>
-    <ul class="flex gap-4 text-lg">
-        <li class="hover:text-red-500 transition"><a href="#">Login</a></li>
-        <li class="hover:text-red-500 transition"><a href="#">Create</a></li>
-    </ul>
-</nav>
+<header class="bg-slate-800 bg-opacity-90">
+    <nav class="container flex items-center justify-between py-2 mb-5">
+        <a href="#" class="text-3xl font-bold ">Todo app</a>
+        <ul class="flex gap-4 text-lg">
+            <li class="transition hover:text-red-500"><a href="#">Login</a></li>
+            <li class="transition hover:text-red-500"><a href="#">Create</a></li>
+        </ul>
+    </nav>
+</header>
