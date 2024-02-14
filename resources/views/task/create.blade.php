6fc92f5d42861a5077390f55fa4fc4f12a95394f
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 12:26:52 -0300

    feat: add toasts on create task form

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layout')
-@section('create-task-form')
 
+@section('create-task-form')
 <form action="{{ route('task.store')}}" method="POST">
     @csrf
     <div class="container">
         <h3 class="mb-4 text-4xl font-bold">Create a task</h3>
-        <div class="flex flex-col  rounded p-4 max-w-[80%] bg-slate-800 gap-2">
+        <div class="flex flex-col rounded p-4 max-w-[80%] bg-slate-800 gap-2 mb-4">
 
             <label for="title" class="text-base font-semibold">Title <span class="text-red-600">*</span></label>
             <input type="text" id="title" name="title" placeholder="Task title..." class="p-2 rounded bg-slate-600">
@@ -19,9 +19,14 @@
             <textarea id="description" name="description" placeholder="A short description..."
                 class="p-2 rounded bg-slate-600 "></textarea>
 
-
             <button type="submit"
                 class="px-2 py-1 mt-2 transition rounded bg-emerald-700 max-w-32 hover:bg-emerald-600">Create</button>
         </div>
+        @if(session('success'))
+        @include('components.successToast')
+        @elseif(session('error'))
+        @include('components.errorToast')
+        @endif
+    </div>
 </form>
 @endsection
