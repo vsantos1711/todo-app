fcceb9309671a4b072c7095dd9f2f3c2c89dd301
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 09:40:12 -0300

    feat: add store function on Task Controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Task;
 class TaskController extends Controller
 {
     public function create() {
         return view('task.create');
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'title' => 'required|min:4|max:50',
+        ]);
+
+        Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
     }
 }
