0d21041ca6e5be8bd507b7c33452ecd87efb354a
vsantos1711 <vsantos067100@gmail.com>
2024-02-13 23:54:29 -0300

    chore: change primary key to UUID

--- database/migrations/2024_02_13_220935_create_task_table.php
+++ database/migrations/2024_02_13_220935_create_task_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('description')->nullable();
             $table->boolean('is_completed')->default(false);
