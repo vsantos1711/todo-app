196c44826946d5c49ff171d8b86ae5e60bec52aa
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 16:05:56 -0300

    style: some changes on success and error  toast

--- resources/views/components/toast/success.blade.php
+++ resources/views/components/toast/success.blade.php
@@ -2,8 +2,8 @@
     class="flex items-center w-full max-w-xs p-4 text-green-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
     role="alert">
     <div
-        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-6 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
             viewBox="0 0 20 20">
             <path
                 d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
@@ -11,13 +11,11 @@
 
         <span class="sr-only">Check icon</span>
     </div>
-    <div class="text-sm font-normal ms-3">
-        <div class="text-sm font-normal ms-3">
-            {{ Session::get('success') }}
-        </div>
+    <div class="text-sm font-normal ms-2">
+        {{ Session::get('success') }}
     </div>
     <button type="button"
-        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-6 w-6 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
         data-dismiss-target="#toast-default" aria-label="Close">
         <span class="sr-only">Close</span>
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -29,8 +27,7 @@
 
 <script>
     const toast = document.getElementById('toast-default');
-    toast.classList.remove('invisible');
-    setTimeout(() => {
-        toast.classList.add('invisible');
-    }, 2000);
+    // setTimeout(() => {
+    //     toast.classList.add('invisible');
+    // }, 2000);
 </script>
