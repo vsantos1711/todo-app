0a7103937be25644bc8df4da231d731bbd4bc93f
vsantos1711 <vsantos067100@gmail.com>
2024-02-14 22:36:17 -0300

    style: change font weight

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,9 @@
-<header class="bg-slate-800 bg-opacity-90">
+<header class="bg-slate-800 bg-opacity-90 h-14">
     <nav class="container flex items-center justify-between py-2 ">
         <a href="/" class="text-3xl font-bold ">Todo App</a>
         <ul class="flex gap-4 text-lg">
-            <li class="transition hover:text-red-500"><a href="#">Login</a></li>
-            <li class="transition hover:text-red-500"><a href="/task/create">Create</a></li>
+            <li class="font-semibold transition hover:text-red-600"><a href="/">Login</a></li>
+            <li class="font-semibold transition hover:text-red-600"><a href="/task/create">Create</a></li>
         </ul>
     </nav>
 </header>
